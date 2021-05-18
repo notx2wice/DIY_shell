02781359b27a36da3ea3b7d0d9413967372346e6
saeyeong <syeongppp@gmail.com>
2021-05-18 20:12:01 +0900

    free_t_hist 필요없는 함수 주석 처리

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -58,7 +58,7 @@ int					main(int ac, char **av, char *env[])
 	signal(SIGINT, sighandler);
 	init_all();
 	print_prompt();
-	free_t_hist(&g_all.thist_start);
+	// free_t_hist(&g_all.thist_start);
 	hist_copy();
 	link_thist_last_now();
 	while (read(1, &c, sizeof(int)))
