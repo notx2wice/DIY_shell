996abe8b9540eee02c69fc7af3f5e79a8682872c
notx2wice <dnrgnl2718@gmail.com>
2021-05-23 00:08:06 +0900

    parsing norm done

--- srcs/parsing2.c
+++ srcs/parsing2.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   parsing2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ukim <ukim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: ukim <ukim@42seoul.kr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 20:20:00 by ukim              #+#    #+#             */
-/*   Updated: 2021/05/21 18:13:43 by ukim             ###   ########.fr       */
+/*   Updated: 2021/05/22 23:42:13 by ukim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	meet_pipe_termi(t_split_one **fc, t_split_one **lc, int flag)
+void	meet_pipe_termi(t_split_one **fc, t_split_one **lc, char c)
 {
-	if (flag == 0)
+	if (c == '|')
 	{
 		(*lc)->pipe_flag = 1;
 		init_cmd(lc, fc);
@@ -66,10 +66,7 @@ int		meet_redir(t_split_one **fc, t_split_one **lc, int *idx, char *cmd)
 			(*lc)->str[(*lc)->top++] = cmd[(*idx)++];
 		}
 		else if (cmd[(*idx) + 1] == '>' || cmd[(*idx) + 1] == '<')
-		{
-			if (cmd[(*idx)] == cmd[(*idx) + 1])
-				return (0);
-		}
+			return (0);
 		else
 		{
 			(*lc)->str[(*lc)->top++] = ' ';
@@ -85,19 +82,14 @@ int		meet_redir(t_split_one **fc, t_split_one **lc, int *idx, char *cmd)
 	return (1);
 }
 
-int		divide_with_ptqd(t_split_one **fc, t_split_one **lc, char *ori)
+int		divide_with_ptqd(t_split_one **fc, t_split_one **lc, char *cmd)
 {
 	int		idx;
-	char	*cmd;
 
-	idx = 0;
-	cmd = ori;
-	while (cmd[idx])
-	{
-		if (cmd[idx] == '|')
-			meet_pipe_termi(fc, lc, 0);
-		else if (cmd[idx] == ';')
-			meet_pipe_termi(fc, lc, 1);
+	idx = -1;
+	while (cmd[++idx])
+		if (cmd[idx] == '|' || cmd[idx] == ';')
+			meet_pipe_termi(fc, lc, cmd[idx]);
 		else if (cmd[idx] == '\'')
 		{
 			if ((meet_q(fc, lc, &idx, cmd) == 0))
@@ -115,8 +107,6 @@ int		divide_with_ptqd(t_split_one **fc, t_split_one **lc, char *ori)
 		}
 		else
 			(*lc)->str[(*lc)->top++] = cmd[idx];
-		idx++;
-	}
 	(*lc)->termi_flag = 1;
 	return (1);
 }
